c4d32dabb6bcecd670ccd259396e86cd7a2f51d2
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-12-06 14:10:49 +0100

    Initial commit

---
+++ src/test/java/nsimat/edu/collectibles/BigStarCollectiblesApplicationTests.java
@@ -0,0 +1,13 @@
+package nsimat.edu.collectibles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigStarCollectiblesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
