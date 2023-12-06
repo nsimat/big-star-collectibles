14d10bde4dbd3a57f69c65119386a6077b08842c
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-12-07 00:14:48 +0100

    Linking other pages to the home page.

--- src/main/java/nsimat/edu/collectibles/controllers/HomeController.java
+++ src/main/java/nsimat/edu/collectibles/controllers/HomeController.java
@@ -1,14 +1,21 @@
 package nsimat.edu.collectibles.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String homeDisplay(){
+    public String homeDisplay(Model model){
 
         return "index";
+    }
+
+    @GetMapping("/getCharacter/{charname}")
+    public String getCharacter(@PathVariable("charname") String charName){
+        return "/characters/" + charName;
     }
 }
