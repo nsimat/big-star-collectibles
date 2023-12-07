528eaa58f414d0e08620e227d4d58373a4da7847
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-12-07 20:55:38 +0100

    Adding a product entity

---
+++ src/main/java/nsimat/edu/collectibles/beans/Product.java
@@ -0,0 +1,26 @@
+package nsimat.edu.collectibles.beans;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    private int id;
+    private String name;
+    private int categoryId;
+    private int robotId;
+    private String color;
+    private String imagePath;
+    private int rating;
+    private int nbrOfReviews;
+    private String description;
+    private int price;
+}
