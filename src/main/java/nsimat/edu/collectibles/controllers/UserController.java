ee58f0c6fa6eb616d64a598f62e4ae8939d3bb97
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-12-08 12:09:34 +0100

    Adding the UserController to display the registration form

---
+++ src/main/java/nsimat/edu/collectibles/controllers/UserController.java
@@ -0,0 +1,14 @@
+package nsimat.edu.collectibles.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/newUser")
+    public String displayRegistrationForm(){
+
+        return "register-user";
+    }
+}
