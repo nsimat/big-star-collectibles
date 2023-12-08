beab7bb8fed5aa929e5ebe566a80ec82e0ccf28c
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-12-09 00:21:14 +0100

    Adding a save code for user

--- src/main/java/nsimat/edu/collectibles/beans/User.java
+++ src/main/java/nsimat/edu/collectibles/beans/User.java
@@ -1,6 +1,8 @@
 package nsimat.edu.collectibles.beans;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
@@ -15,26 +17,29 @@ import java.util.Date;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @DateTimeFormat(pattern = "YYYY-MM-dd")
-    /*@NotNull(message = "Date of birth cannot be empty")
-    @Past(message = "Date of birth cannot be a future date")*/
+    @NotNull(message = "Date of birth cannot be empty.")
+    @Past(message = "Date of birth cannot be a future date.")
     private Date dateOfBirth;
 
-    /*@NotEmpty(message = "Email cannot be empty")
-    @Email(message = "Should be a well-formed email address")*/
+    @NotEmpty(message = "Email cannot be empty.")
+    @Email(message = "Should be a well-formed email address.")
     private String email;
 
-    //@NotEmpty(message = "Select at least one favorite character")
+    @NotEmpty(message = "Select at least one favorite character.")
     private String favoriteCollection;
 
-    //@NotEmpty(message = "Gender cannot be empty")
+    @NotEmpty(message = "Gender cannot be empty.")
     private String gender;
+
     private String message;
 
-    /*@NotEmpty(message = "Name cannot be empty.")
-    @Size(min = 1, max = 20, message = "Size must be between 1 and 20")*/
+    @NotEmpty(message = "Name cannot be empty.")
+    @Size(min = 1, max = 20, message = "Size must be between 1 and 20.")
     private String name;
+
     private boolean newsLetter;
 }
--- src/main/java/nsimat/edu/collectibles/controllers/UserController.java
+++ src/main/java/nsimat/edu/collectibles/controllers/UserController.java
@@ -2,6 +2,7 @@ package nsimat.edu.collectibles.controllers;
 
 import jakarta.validation.Valid;
 import nsimat.edu.collectibles.beans.User;
+import nsimat.edu.collectibles.dao.UserRepository;
 import nsimat.edu.collectibles.validators.UserValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,16 +16,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
 
-    private final UserValidator userValidator;
+    //private final UserValidator userValidator;
+    private final UserRepository userRepository;
 
-    public UserController(UserValidator userValidator) {
-        this.userValidator = userValidator;
+    public UserController(UserRepository userRepository) {
+        //this.userValidator = userValidator;
+        this.userRepository = userRepository;
     }
 
-    @InitBinder
+    /*@InitBinder
     public void bindUser(WebDataBinder binder){
         binder.addValidators(this.userValidator);
-    }
+    }*/
 
     @GetMapping("/newUser")
     public String displayRegistrationForm(Model model){
@@ -37,7 +40,15 @@ public class UserController {
     public String saveUser(@ModelAttribute("user") @Valid User user,
                            BindingResult result,
                            Model model){
-        //validate user and save it to DB
+
+        if(result.hasErrors()) {
+            return "register-user";
+        }
+
+        var userSaved = userRepository.save(user);
+        if(userSaved != null){
+            model.addAttribute("userSaved", true);
+        }
         return "register-user";
     }
 }
