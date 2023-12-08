d66a2b37cef362ac42ee7bbe44aa51ffe54ecd9c
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-12-08 22:41:33 +0100

    Adding a UserRepository

---
+++ src/main/java/nsimat/edu/collectibles/dao/UserRepository.java
@@ -0,0 +1,12 @@
+package nsimat.edu.collectibles.dao;
+
+import nsimat.edu.collectibles.beans.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Override
+    <S extends User> S save(S entity);
+}
