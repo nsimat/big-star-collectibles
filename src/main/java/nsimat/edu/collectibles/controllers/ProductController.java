ad9ff2aa0e06bb60094683e9267f9ba660345bc3
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-12-10 23:36:11 +0100

    Adding method for fetching all products

--- src/main/java/nsimat/edu/collectibles/controllers/ProductController.java
+++ src/main/java/nsimat/edu/collectibles/controllers/ProductController.java
@@ -3,20 +3,30 @@ package nsimat.edu.collectibles.controllers;
 import nsimat.edu.collectibles.beans.Product;
 import nsimat.edu.collectibles.dao.ProductRepository;
 import nsimat.edu.collectibles.services.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.List;
+import java.util.concurrent.Executor;
 
 @Controller
 public class ProductController {
 
+    private final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
+    private final Executor asyncExecutor;
+
+    public ProductController(ProductService productService, Executor asyncExecutor) {
         this.productService = productService;
+        this.asyncExecutor = asyncExecutor;
     }
 
     @PostMapping("/search")
@@ -27,5 +37,26 @@ public class ProductController {
         model.addAttribute("searchedFor", keyword);
 
         return "search-results";
+    }
+
+    @GetMapping("/getAllProducts")
+    public DeferredResult<String> getAllProducts(Model model){
+        DeferredResult<String> deferredResult = new DeferredResult<>();
+        asyncExecutor.execute(() -> {
+            model.addAttribute("products", getProducts());
+            deferredResult.setResult("product-list");
+        });
+        return deferredResult;
+    }
+
+    private Iterable<Product> getProducts() {
+
+        logger.info("Getting all products as we are on the spring executor thread");
+        try{
+            Thread.sleep(6000);
+        }catch(InterruptedException exception){
+            throw new RuntimeException("Something happened during waiting for products!!!");
+        }
+        return null;
     }
 }
