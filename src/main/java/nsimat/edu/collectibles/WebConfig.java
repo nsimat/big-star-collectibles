4968ba07c938c549f1433bf929c1ae6816c4c0ac
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-12-12 13:04:15 +0100

    Adding config class for interceptor class

---
+++ src/main/java/nsimat/edu/collectibles/WebConfig.java
@@ -0,0 +1,16 @@
+package nsimat.edu.collectibles;
+
+import nsimat.edu.collectibles.interceptors.ApplicationInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ApplicationInterceptor());
+    }
+}
