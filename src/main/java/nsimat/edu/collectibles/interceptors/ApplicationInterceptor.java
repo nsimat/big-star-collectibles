7f2bfc764cfdd72c6c74918ce0645799913ffb92
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2023-12-12 02:05:47 +0100

    Adding and registering an interceptor class

---
+++ src/main/java/nsimat/edu/collectibles/interceptors/ApplicationInterceptor.java
@@ -0,0 +1,33 @@
+package nsimat.edu.collectibles.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ApplicationInterceptor implements HandlerInterceptor {
+
+    private final Logger LOG = LoggerFactory.getLogger(ApplicationInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+        LOG.info("in pre-handle on interceptor");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response,
+                           Object handler, ModelAndView modelAndView) throws Exception {
+        LOG.info("in post-handle on interceptor");
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        LOG.info("in after-completion on interceptor");
+    }
+}
